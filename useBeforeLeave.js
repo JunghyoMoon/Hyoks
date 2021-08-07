596639594596c8e15bdf76768b4b1e66481232ba
JungHyoMoon <dsinhome70@gmail.com>
2021-08-07 16:51:59 +0900

    usebeforeleave is done

---
+++ useBeforeLeave.js
@@ -0,0 +1,15 @@
+const useBeforeLeave = (onBefore) => {
+    if (!onBefore || typeof onBefore !== "function") {
+        return;
+    }
+    const handle = (e) => {
+        const { clientY } = e;
+        if (clientY <= 0) {
+            onBefore();
+        }
+    };
+    useEffect(() => {
+        document.addEventListener("mouseleave", handle);
+        return () => document.removeEventListener("mouseleave", handle);
+    }, []);
+};
