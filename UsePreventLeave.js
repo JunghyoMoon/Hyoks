0c98f4d257d38ef1f044fe6258cc945f6d1d8696
JungHyoMoon <dsinhome70@gmail.com>
2021-08-07 15:09:38 +0900

    usepreventleave is done

---
+++ UsePreventLeave.js
@@ -0,0 +1,12 @@
+const usePreventLeave = () => {
+    const listener = (e) => {
+        e.preventDefault();
+        // only chrome?
+        e.returnValue = "";
+    };
+    const enablePrevent = () =>
+        window.addEventListener("beforeunload", listener);
+    const disablePrevent = () =>
+        window.removeEventListener("beforeunload", listener);
+    return { enablePrevent, disablePrevent };
+};
